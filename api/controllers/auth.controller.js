06c7f7ac574970b384469f70bd105f653b627c77
g3vind <g3vind@gmail.com>
2023-12-24 22:44:35 +0530

    signin api route , cookie, jwt

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
+// ----------------SIGN UP---------------------------
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   //   Using bcryptjs to hash the password
@@ -13,6 +16,30 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ messgae: "User Created Successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// ----------------------SIGN IN-------------------
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User Not Found!"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid Credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000);
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -72,9 +72,7 @@ export default function SignUp() {
           <span className="text-blue-500">Sign in</span>
         </Link>
       </div>
-      <p className="text-red-800 mt-5 text-center">
-        {error && "Something went wrong!"}
-      </p>
+      <p className="text-red-800 mt-5">{error && "Something went wrong!"}</p>
     </div>
   );
 }
